5b7ea9db505c830f2c245333d3b726cabab527a9
Web前端-吴溥毅 <wupuyi@foxmail.com>
2018-01-04 09:58:14 +0800

    chapter

--- learn-vue2/002advanced/chapter009-app/table-component/index.js
+++ learn-vue2/002advanced/chapter009-app/table-component/index.js
@@ -13,6 +13,15 @@ var app = new Vue({
                 key: 'age',
                 // 选填字段，说明是否需要排序
                 sortable: true
+            },
+            {
+                title: '出生日期',
+                key: 'birthday',
+                sortable: true
+            },
+            {
+                title: '地址',
+                key: 'address'
             }
         ],
         data: [
@@ -21,7 +30,32 @@ var app = new Vue({
                 age: 18,
                 birthday: '1999-02-21',
                 address: '北京市朝阳区芍药居'
+            },{
+                name: '张小刚',
+                age: 25,
+                birthday: '1992-01-23',
+                address: '北京市海淀区西二旗'
+            },{
+                name: '李小红',
+                age: 30,
+                birthday: '1987-11-10',
+                address: '上海市浦东新区世纪大道'
+            },{
+                name: '周小伟',
+                age: 26,
+                birthday: '1991-10-10',
+                address: '深圳市南山区深南大道'
             }
         ]
+    },
+    methods: {
+        handleAddData: function() {
+            this.data.push({
+                name: '刘小天',
+                age: 19,
+                birthday: '1998-05-30',
+                address: '北京市东城区东直门'
+            });
+        }
     }
-})
+});
--- learn-vue2/002advanced/chapter009-app/table-component/table.js
+++ learn-vue2/002advanced/chapter009-app/table-component/table.js
@@ -22,7 +22,7 @@ Vue.component('vTable', {
     },
     methods: {
         makeColumns: function() {
-            this.currentColums = this.columns.map(function(col, index) {
+            this.currentColumns = this.columns.map(function(col, index) {
                 // 添加一个字段标识当前列排序的状态，后续使用
                 col._sortType = 'normal';
                 // 添加一个字段标识当前列在数组中的索引，后续使用
@@ -36,6 +36,27 @@ Vue.component('vTable', {
                 row._index = index;
                 return row;
             })
+        },
+        handleSortByAsc: function(index) {
+            var key = this.currentColumns[index].key;
+            this.currentColumns.forEach(function(col) {
+                col._sortType = 'normal';
+            });
+            this.currentColumns[index]._sortType = 'asc';
+
+            this.currentData.sort(function(a ,b) {
+                return a[key] > b[key] ? 1 : -1;
+            });
+        },
+        handleSortByDesc: function(index) {
+            var key = this.currentColumns[index].key;
+            this.currentColumns.forEach(function(col) {
+                col._sortType = 'normal';
+            });
+            this.currentColumns[index].sortType = 'desc';
+            this.currentData.sort(function(a, b) {
+                return a[key] < b[key] ? 1 : -1;
+            });
         }
     },
     mounted() {
@@ -43,9 +64,68 @@ Vue.component('vTable', {
         this.makeColumns();
         this.makeData();
     },
+    watch: {
+        data: function() {
+            this.makeData();
+            var sortedColumn = this.currentColumns.filter(function(col) {
+                return col._sortType !== 'normal';
+            });
+
+            if (sortedColumn.length > 0) {
+                if(sortedColumn[0]._sortType === 'asc') {
+                    this.handleSortByAsc(sortedColumn[0]._index);
+                } else {
+                    this.handleSortByDesc(sortedColumn[0]._index);
+                }
+            }
+        }
+    },
     render: function(h) {
+        // 这里的h就是createElement,只是换了个名称
+        var _this = this;
         var ths = [];
+
+        this.currentColumns.forEach(function(col, index) {
+            if(col.sortable) {
+                ths.push(h('th',[
+                    h('span', col.title),
+                    // 升序
+                    h('a', {
+                        class: {
+                            on: col._sortType === 'asc'
+                        },
+                        on: {
+                            click: function() {
+                                _this.handleSortByAsc(index)
+                            }
+                        }
+                    }, '↑'),
+                    // 降序
+                    h('a', {
+                        class: {
+                            on: col._sortType === 'desc'
+                        },
+                        on: {
+                            click: function() {
+                                _this.handleSortByDesc(index)
+                            }
+                        }
+                    }, '↓')
+                ]));
+            } else {
+                ths.push(h('th', col.title))
+            }
+        });
+
         var trs = [];
+        this.currentData.forEach(function(row) {
+            var tds = [];
+            _this.currentColumns.forEach(function(cell) {
+                tds.push(h('td', row[cell.key]));
+            });
+            trs.push(h('tr', tds));
+        })
+
         return h('table', [
             h('thead',[
                 h('tr', ths)
