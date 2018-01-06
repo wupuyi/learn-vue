309a7fd4ded66f75832a3bd7808b602a6bf3f6b5
Web前端-吴溥毅 <wupuyi@foxmail.com>
2018-01-06 16:23:41 +0800

    chapter13 zhihuDaily

---
+++ learn-vue2/003 application/chapter013/zhihuDaily/app.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="daily">
+        <v-title title='123'></v-title>
+        <v-button></v-button>
+    </div>
+</template>
+<script>
+    import vTitle from './title.vue';
+    import vButton from './button.vue';
+
+    export default {
+
+        components: {
+            vTitle,
+            vButton
+        },
+        methods: {
+            handleClick (e) {
+                console.log(e);
+            }
+        }
+    }
+</script>
