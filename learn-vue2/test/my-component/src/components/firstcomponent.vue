a308da214931492942fea896a28d0d509fe571cb
Web前端-吴溥毅 <wupuyi@foxmail.com>
2018-01-10 17:33:17 +0800

    commit

---
+++ learn-vue2/test/my-component/src/components/firstcomponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="firstcompont">
+        <h1>I am a Title.</h1>
+        <a>written by {{ author }}</a>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+      return{
+          author: "我是世界之王"
+      }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
